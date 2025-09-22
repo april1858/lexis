6a270e8ffaea1230ba6c3958ef8988deb755b87a
vi <april1858@yandex.ru>
2025-09-22 11:52:51 +0700

    tree

--- main.go
+++ main.go
@@ -1,70 +1,60 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"regexp"
-	"strings"
 )
 
-/*
-golang лексический парсинг строки логического выражения
-*/
-
-func main() {
-	// построчное считываение данных из консоли
-	var input string
-	vars := make(map[string]string)
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if !strings.Contains(line, ";") {
-			input = strings.ReplaceAll(line, " ", "")
-			break
-		}
-		line = line[:len(line)-1]
-		v := strings.Split(line, "=")
-		if v[1] == "" {
-			v[1] = "False"
-		}
-		val := strings.ToLower(v[1])
-		vars[v[0]] = val
-	}
-
-	fmt.Println(input)
-
-	re := regexp.MustCompile(`[\w]+`) // \w+ находит одну или более букв, цифр или нижнее подчеркивание
-
-	tokens := re.FindAllString(input, -1)
-
-	fmt.Println("Токены:")
-	for _, token := range tokens {
-		fmt.Println(token)
-	}
-
-	fmt.Println(vars)
-
-	fmt.Println(operations(input))
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("[error]")
-	}
+type Node struct {
+	Parent *Node
+	Lnode  *Node
+	Rnode  *Node
+	Value  string
 }
 
-func operations(line string) []string {
-	op := make([]string, 0)
-	if strings.Contains(line, ")not(") {
-		op = append(op, "not")
+func NewNode(v string) *Node {
+	n := Node{
+		Value: v,
 	}
-	if strings.Contains(line, ")and(") {
-		op = append(op, "and")
+	return &n
+}
+
+func main() {
+	root := NewNode("")
+
+	current := root
+
+	slStr := []string{"a", "and", "b", "or"}
+
+	re, err := regexp.Compile(`and|not|or|xor`)
+	if err != nil {
+		fmt.Println("error re - ", err)
 	}
-	if strings.Contains(line, ")or(") {
-		op = append(op, "or")
+
+	for i := 0; i < len(slStr); i++ {
+		if !re.MatchString(slStr[i]) {
+
+			if current.Lnode == nil {
+				current.Lnode = NewNode(slStr[i])
+				current.Lnode.Parent = current
+			} else if current.Rnode == nil {
+				current.Rnode = NewNode(slStr[i])
+				current.Rnode.Parent = current
+			}
+		} else {
+			if current.Value == "" {
+				current.Value = slStr[i]
+			} else {
+				current.Parent = NewNode(slStr[i])
+				current.Parent.Lnode = current
+				current = current.Parent
+				root = current
+			}
+		}
 	}
-	if strings.Contains(line, ")xor(") {
-		op = append(op, "xor")
-	}
-	return op
+
+	fmt.Println(root)
+	fmt.Println(root.Lnode)
+	fmt.Println(root.Rnode)
+
 }
